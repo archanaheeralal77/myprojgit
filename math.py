b2826b8f1ff58e7625de80a39ce89e3168389196
archanaheeralal77 <127080874+archanaheeralal77@users.noreply.github.com>
2023-05-04 11:26:02 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,29 @@
+# input a float value for variable a here
+# input a float value for variable b here
+
+# output the result of addition here
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+add = int(x) + int(y)
+
+print("The sum is: ", add) 
+
+subb = int(x) - int(y)
+
+print("The sum is: ", subb) 
+
+mull = int(x) * int(y)
+
+print("The sum is: ", mull) 
+
+divv = int(x) / int(y)
+
+print("The sum is: ", divv) 
+
+
+print("\nThat's all, folks!")
---
+++ power1.py
@@ -0,0 +1,4 @@
+anything = input("Enter a number: ")
+powerval = input ("Enter Power to value: ")
+something = anything ** powerval
+print(anything, powerval, something)
